a572691534ebc34d4d8cbfd788f84ce9ef11f870
Lucas Meneses <lucasnemeses01@gmail.com>
2023-10-12 22:22:59 -0300

    update task

--- src/main/java/com/lucasmeneses/todolist/task/TaskController.java
+++ src/main/java/com/lucasmeneses/todolist/task/TaskController.java
@@ -8,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasmeneses.todolist.utils.Utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -46,5 +50,13 @@ public class TaskController {
     public List<TaskModel> list(HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         return this.taskRepository.findByIdUser((UUID) idUser);
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        var task = this.taskRepository.findById(id).orElse(null);
+        Utils.copyNonNUllProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
